570f21bf5561dfb67ff7f33d11773edfea8e722a
Jeremy Saenz <jeremy.saenz@gmail.com>
2024-04-23 18:15:39 -0700

    Slightly better styling

--- src/components/question.tsx
+++ src/components/question.tsx
@@ -21,10 +21,7 @@ export function Question(props: Props) {
 
   return (
     <>
-      <div class="flex items-center w-full">
-        <span class="flex-grow text-2xl font-medium text-zinc-400">
-          Question {current.id}
-        </span>
+      <div class="absolute top-8 right-8">
         <Show when={!props.showAnswer}>
           <span class="font-semibold text-4xl bg-zinc-800 border-[6px] border-purple-500 rounded-full p-4 w-20 h-20 flex items-center justify-center">
             {props.seconds}
@@ -32,7 +29,7 @@ export function Question(props: Props) {
         </Show>
       </div>
       <div class="flex flex-grow items-center justify-center w-full">
-        <div class="w-full flex gap-12">
+        <div class="w-full items-center justify-center flex gap-12">
           <img
             src="/people/jeremy.jpeg"
             class="rounded-full w-36 h-36 border-[2px] border-zinc-800 self-end"
